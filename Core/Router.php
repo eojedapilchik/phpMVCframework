3a76efb78f1c9ff2a79b647ce4d786b8a2146d9f
unknown <eojedapilchik@gmail.com>
2020-07-03 19:46:36 -0400

    Added params functionality and routes in frontcontroller

--- Core/Router.php
+++ Core/Router.php
@@ -7,27 +7,54 @@
 class Router
 {
 
-protected $routes =[]; //Array of routes (routing table)
-}
+    protected $routes =[]; //Array of routes (routing table)
 
-/*
- Adds a route to the routing tables
-    string  $route    route URL
-    array   $params   Parameters (controller, action, etc)
-*/
+    protected $params =[]; //parameteres from the mateched route
 
-public function addRoute($route, $params)
-{
-  $this->routes[$route]=$params;
-}
+    /*
+    Adds a route to the routing tables
+        string  $route    route URL
+        array   $params   Parameters (controller, action, etc)
+    */
+    public function addRoute($route, $params)
+    {
+      $this->routes[$route]=$params;
+    }
 
-/*
-Gets all the routes from the router tables
-  returns array
-*/
+    /*
+    Gets all the routes from the router tables
+      returns array
+    */
+    public function getRoutes()
+    {
+      return $this->routes;
+    }
 
-public function getRoutes()
-{
-  return $this->routes;
+    /*
+    Gets the params from matched route/url - controller and actions
+      returns array   controller,action (items)
+
+    */
+    public function getParams()
+    {
+      return $this->params;
+    }
+
+    /*
+    Match the url to the routes in the routing tables
+        string  $url  the route urldecode
+        returns boolean true if match foun, false otherwise
+    */
+    public function match($url)
+    {
+      foreach ($this->routes as $route => $params){
+        if($url == $route){
+          $this->params= $params;
+          return true;
+        }
+      }
+      return false;
+    }
+
 }
 ?>
--- public/index.php
+++ public/index.php
@@ -9,8 +9,26 @@ require '../Core/Router.php';
 
 $router = new Router();
 
-echo get_class($router);
+//echo get_class($router);
+//echo 'Requested URL 1 = "'.$_SERVER['QUERY_STRING'].'"';
 
-echo 'Requested URL 1 = "'.$_SERVER['QUERY_STRING'].'"';
+// Add the routes
+$router->addRoute('',['controller' => 'Home', 'action' =>'index']);
+$router->addRoute('posts',['controller'=>'Posts', 'action'=>'index']);
+$router->addRoute('posts/new',['controller'=>'Posts', 'action'=>'new']);
+
+// display the routing table
+// echo '<pre>';
+// var_dump ($router->getRoutes());
+// echo '</pre>';
+
+$url =$_SERVER['QUERY_STRING'];
+if($router->match($url)){
+   echo '<pre>';
+   var_dump ($router->getParams());
+   echo '</pre>';
+}else{
+  echo "No route found for URL '$url'";
+}
 
 ?>
