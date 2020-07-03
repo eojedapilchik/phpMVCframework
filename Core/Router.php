11b4e37593275b0dbd6dba0048adcb2f35c33cd0
unknown <eojedapilchik@gmail.com>
2020-07-03 19:16:21 -0400

    Added router methods Add and Get

--- Core/Router.php
+++ Core/Router.php
@@ -6,7 +6,28 @@
 
 class Router
 {
-  
+
+protected $routes =[]; //Array of routes (routing table)
 }
 
+/*
+ Adds a route to the routing tables
+    string  $route    route URL
+    array   $params   Parameters (controller, action, etc)
+*/
+
+public function addRoute($route, $params)
+{
+  $this->routes[$route]=$params;
+}
+
+/*
+Gets all the routes from the router tables
+  returns array
+*/
+
+public function getRoutes()
+{
+  return $this->routes;
+}
 ?>
